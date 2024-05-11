1c3cacfeb24cb95ce90282a009d51341c2d54184
eldor pardayev <eldorpardayev18@gmail.com>
2024-05-11 21:30:46 +0500

    INFRA: Initialize Unit Test Project

---
+++ Sheenam.Api.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,13 @@
+﻿//===================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use Comfort an Peace 
+//===================================
+
+namespace Sheenam.Api.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() => Assert.True(true);
+    }
+}
