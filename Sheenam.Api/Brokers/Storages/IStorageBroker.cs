92101daef84da15c753513d6b32fc7204a4ec366
eldor pardayev <eldorpardayev18@gmail.com>
2024-05-12 12:28:25 +0500

    BROKERS: Insert Guest

---
+++ Sheenam.Api/Brokers/Storages/IStorageBroker.cs
@@ -0,0 +1,11 @@
+﻿//===================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use Comfort an Peace 
+//===================================
+
+namespace Sheenam.Api.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+    }
+}
